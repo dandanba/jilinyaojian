11a43d8070b2d7ed95de5ea2accb953cb8910cb9
dandanba <13641194007@139.com>
2015-11-29 20:06:00 +0800

    lib -> libs

--- gen/com/acctrue/jlyj/R.java
+++ gen/com/acctrue/jlyj/R.java
@@ -17,25 +17,105 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int a=0x7f020000;
+        public static final int b=0x7f020001;
+        public static final int bj1_03=0x7f020002;
+        public static final int bj2_02=0x7f020003;
+        public static final int bj3=0x7f020004;
+        public static final int bj3_02=0x7f020005;
+        public static final int bj4_01=0x7f020006;
+        public static final int bj4_02=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int icon=0x7f020009;
+        public static final int item_back=0x7f02000a;
+        public static final int item_back1=0x7f02000b;
+        public static final int item_background=0x7f02000c;
+        public static final int nfc=0x7f02000d;
+        public static final int no_image=0x7f02000e;
+        public static final int tb1=0x7f02000f;
+        public static final int tb10=0x7f020010;
+        public static final int tb2=0x7f020011;
+        public static final int tb3=0x7f020012;
+        public static final int tb4=0x7f020013;
+        public static final int tb5=0x7f020014;
+        public static final int tb6=0x7f020015;
+        public static final int tb7=0x7f020016;
+        public static final int tb8=0x7f020017;
+        public static final int tb9=0x7f020018;
+        public static final int wb_03=0x7f020019;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090020;
+        public static final int batch=0x7f09001b;
+        public static final int batchTitle=0x7f09001d;
+        public static final int batch_input_et=0x7f09000b;
+        public static final int blank=0x7f09001c;
+        public static final int data=0x7f09001a;
+        public static final int date_input_et=0x7f09000c;
+        public static final int dcoument_tv_1=0x7f090011;
+        public static final int dcoument_tv_2=0x7f090012;
+        public static final int dcoument_tv_3=0x7f090013;
+        public static final int dcoument_tv_4=0x7f090014;
+        public static final int dcoument_tv_5=0x7f090015;
+        public static final int dcoument_tv_6=0x7f090016;
+        public static final int dcoument_tv_7=0x7f090017;
+        public static final int dcoument_tv_8=0x7f090018;
+        public static final int fact_in_edit_text=0x7f09000e;
+        public static final int js_tv=0x7f09000d;
+        public static final int js_tv_1=0x7f09000f;
+        public static final int lv=0x7f090019;
+        public static final int menu=0x7f090003;
+        public static final int menuTitle=0x7f090002;
+        public static final int menu_sufaceView=0x7f090001;
+        public static final int method_switch=0x7f090010;
+        public static final int other_menu=0x7f09000a;
+        public static final int other_menuTitle=0x7f090009;
+        public static final int other_view=0x7f090008;
+        public static final int pageMenu=0x7f09001e;
+        public static final int setting_cropid_editview=0x7f090006;
+        public static final int setting_tts_editview=0x7f090005;
+        public static final int setting_username_editview=0x7f090007;
+        public static final int third_btn=0x7f09001f;
+        public static final int title=0x7f090004;
+        public static final int view=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_opinion=0x7f030001;
+        public static final int activity_other_menu=0x7f030002;
+        public static final int activity_pop_add_info=0x7f030003;
+        public static final int activity_pop_js=0x7f030004;
+        public static final int decument_item=0x7f030005;
+        public static final int head_item=0x7f030006;
+        public static final int head_item2=0x7f030007;
+        public static final int head_item3=0x7f030008;
+        public static final int layout_load=0x7f030009;
+        public static final int layout_main=0x7f03000a;
+        public static final int layout_order=0x7f03000b;
+        public static final int layout_order2=0x7f03000c;
+        public static final int layout_order3=0x7f03000d;
+        public static final int layout_othermain=0x7f03000e;
+        public static final int lv_item=0x7f03000f;
+        public static final int lv_item2=0x7f030010;
+        public static final int lv_item3=0x7f030011;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int other_menu=0x7f080001;
+        public static final int pop_add_info=0x7f080002;
+        public static final int pop_j=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_other_menu=0x7f060005;
+        public static final int title_activity_pop_add_info=0x7f060003;
+        public static final int title_activity_pop_js=0x7f060004;
     }
     public static final class style {
         /** 
@@ -59,10 +139,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int popTheme=0x7f070002;
+    }
+    public static final class xml {
+        public static final int nfc_tech_filter=0x7f040000;
     }
 }
