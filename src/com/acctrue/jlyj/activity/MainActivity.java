8365ddf1aeea9625a8883c1014578cd292127029
dandanba <13641194007@139.com>
2015-11-29 21:23:01 +0800

    update .gitignore

--- src/com/acctrue/jlyj/activity/MainActivity.java
+++ src/com/acctrue/jlyj/activity/MainActivity.java
@@ -6,15 +6,13 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.layout_main);
-		
-		
-		
-		
+		// TODO …Ë÷√º‡Ã˝ ≤¢«“∑¢ÀÕEvent
+
 	}
 
 }
